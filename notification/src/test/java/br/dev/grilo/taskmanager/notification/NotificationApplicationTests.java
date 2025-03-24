8ce608007f1aef6edbda00e0b8a05a405c003bd9
Matheus Grilo <matheusgrilo@live.com>
2025-03-24 20:02:45 -0300

    feat: initialize Spring Boot application with Gradle setup and basic configuration

---
+++ notification/src/test/java/br/dev/grilo/taskmanager/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.grilo.taskmanager.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
